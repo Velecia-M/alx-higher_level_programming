8a8a745980dd1c25a2093f47a9320a6e15777e24
Velecia-M <veleciamokoena@gmail.com>
2024-01-11 11:23:15 -0800

    2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    arg = sys.argv
+    n_of_el = len(arg) - 1
+    if n_of_el > 1:
+        print("{} arguments:".format(n_of_el))
+        for x in range(1, n_of_el +1):
+            print("{}: {}".format(x, arg[x]))
+    elif n_of_el == 0:
+        print("{} arguments".format(n_of_el))
+    else:
+        print("{} arguments:".format(n_of_el))
+        print("{}: {}".format(x, arg[1]))
