f85329711c1c60571df4710c2a853ec68caaa966
Velecia-M <veleciamokoena@gmail.com>
2024-01-12 10:22:20 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,12 @@
 if __name__ == "__main__":
     import sys
     n_of_el = len(sys.argv) - 1
-    if n_of_el > 1:
-        print("{} arguments:".format(n_of_el))
-        for x in range(1, n_of_el +1):
-            print("{}: {}".format(x, arg[x]))
-    elif n_of_el == 0:
-        print("{} arguments.".format(n_of_el))
+
+    if n_of_el == 0:
+        print("0 arguments.")
+    elif n_of_el == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(n_of_el))
-        print("{}: {}".format(n_of_el, arg[1]))
+    for x in range(n_of_el):
+        print("{}: {}".format(x + 1, sys.argv[x + 1]))
