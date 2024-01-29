451398656a5cb22c51b3b15b491472fa006143ef
Velecia-M <veleciamokoena@gmail.com>
2024-01-29 03:29:21 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    
+    from hidden_4 import *
+
+
     names = dir()
     for x in range(0, len(names)):
         if names[x][:2] != "__":
