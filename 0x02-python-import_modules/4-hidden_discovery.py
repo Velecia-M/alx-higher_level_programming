ee597efeba378ce15b5d74bd83874c27beb9f47b
Velecia-M <veleciamokoena@gmail.com>
2024-01-29 03:20:17 -0800

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import hidden_4
     
-    names = dir(hidden_4)
-    for x in names:
-        if x[:2] != "__":
-            print(x)
+    names = dir()
+    for x in range(0, len(names)):
+        if names[x][:2] != "__":
+            print("".format(names[x]))
