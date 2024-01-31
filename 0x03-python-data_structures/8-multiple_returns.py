3231f532ec4653894de8cb808f0f361a14ef12e5
Velecia-M <veleciamokoena@gmail.com>
2024-01-31 02:24:33 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     sen_to_tuple = ()
+    y = sentence[0]
     x = len(sentence)
     if x == 0:
         sen_to_tuple = 0, "None"
     else:
-        sen_to_tuple = (x, sentence[0])
+        sen_to_tuple = x, y
         return sen_to_tuple
