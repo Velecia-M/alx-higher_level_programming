8b11a43597952967694be27443f762373f794241
Velecia-M <veleciamokoena@gmail.com>
2024-01-31 01:11:57 -0800

    1-element_at.py

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     x = len(my_list)
     if idx < 0:
         return None
-    elif idx > x:
+    elif idx >= x:
         return None
     else:
         return my_list[idx]
