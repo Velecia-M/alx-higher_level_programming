95811123be0d54c8301f6dea712c025d0bc0dc13
Velecia-M <veleciamokoena@gmail.com>
2024-02-02 00:36:32 -0800

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""Defining a class"""
+
+
+class Square:
+    """creating a class Square"""
+
+    def __init__(self, size=0):
+        """Initializing a new square
+
+        Args:
+            size (int): size of square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """sets the current size of square"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Returns the current area of square"""
+        return (self.__size * self.__size)
