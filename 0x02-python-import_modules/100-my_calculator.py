cac66c63a957982f79800a6d5d891400ac9bd643
Velecia-M <veleciamokoena@gmail.com>
2024-04-18 12:42:21 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,19 +4,19 @@ if __name__ == "__main__":
     from sys import argv
     len_of_args = len(argv)
     if len_of_args != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
         exit(1)
     a = int(argv[1])
     operator = int(argv[2])
     b = int(argv[3])
     if operator == '+':
-        print(f"{a} {operator} {b} = (add(a, b))")
+        print(f"{a} {operator} {b} = (add(a, b))\n")
     elif operator == '-':
-        print(f"{a} {operator} {b} = (sub(a, b))")
+        print(f"{a} {operator} {b} = (sub(a, b))\n")
     elif operator == '*':
-        print(f"{a} {operator} {b} = (mul(a, b))")
+        print(f"{a} {operator} {b} = (mul(a, b))\n")
     elif operator == '/':
-        print(f"{a} {operator} {b} = (div(a, b))")
+        print(f"{a} {operator} {b} = (div(a, b))\n")
     else:
-        print("Unknown operator. Available operators: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /\n")
         exit(1)
