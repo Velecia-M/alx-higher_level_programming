c6ea5cbbaa6b81c098752c7c6ee9ce0bae0d7e6a
Velecia-M <veleciamokoena@gmail.com>
2024-04-23 06:06:54 -0700

    models/base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """Module for a Base Class."""
 
+
 class Base:
     """Represents a Base class"""
 
     __nb_objects = 0
+    
     def __init__(self, id=None):
         if id is not None:
             self.id = id
