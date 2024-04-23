a637f5ffe5b4834eb50e78c55ff679f1595e8fe1
Velecia-M <veleciamokoena@gmail.com>
2024-04-23 07:31:05 -0700

    models/rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Module for a Rectangle class. """
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Represents a Rectangle class. """
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """ Constructor """
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """ Width of the Rectangle. """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Height of the Rectangle. """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """ x coordinate of the Rectangle. """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """ y coordinate of the Rectangle. """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
