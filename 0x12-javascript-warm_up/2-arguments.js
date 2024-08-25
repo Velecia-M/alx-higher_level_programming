73816c560fd9ad255e4daad140fe2d91175dfde0
Velecia-M <veleciamokoena@gmail.com>
2024-08-25 13:11:16 -0700

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,7 @@
 /* A script that prints a message depending of the number of arguments passed
  */
 const args = process.argv;
+
 if (args.length <= 2) {
   console.log('No argument');
 } else if (args.length === 3) {
