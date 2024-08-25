39bc83fc08857a618412a279b3978fdec7922a0d
Velecia-M <veleciamokoena@gmail.com>
2024-08-25 08:12:30 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,7 +3,8 @@
 
 The first line: “C is fun”
 The second line: “Python is cool”
-The third line: “JavaScript is amazing”*/
+The third line: “JavaScript is amazing”
+*/
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
