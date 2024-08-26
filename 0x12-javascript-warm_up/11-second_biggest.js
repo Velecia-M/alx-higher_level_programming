fb7f4b951fe11bc5897d130d8ac9264384518c67
Velecia-M <veleciamokoena@gmail.com>
2024-08-26 15:36:43 -0700

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/* A script that searches the second biggest integer in the list of arguments.
+ */
+const args = process.argv;
+
+if (args.length <= 3) {
+  let int2 = parseInt(args[2]);
+  let highest = parseInt(args[3]);
+  for (let i = 2; i < args.length; i++) {
+    const num = parseInt(args[i]);
+    if (num > highest) {
+      int2 = highest;
+      highest = num;
+    } else if (num > int2 && num < highest) {
+      int2 = num;
+    }
+  }
+  console.log(int2);
+}
